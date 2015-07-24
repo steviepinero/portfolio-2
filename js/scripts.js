ff7f27b54c8236ccec568d26504ff78ea8079d1d
Steven Chen <steven20904@yahoo.com>
2015-07-24 03:09:05 -0400

    Refactor script.js
    Refactor script.js comments.

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,15 @@ $(document).ready(function() {
 	$(".message-box").css("background-color", "yellow");
 	$("#button").on("click", function(){
 	// now we're going to work in here
-	// all the rest of our code still goes up here
 	console.log("clicked");
 	var comment = $(".comment-box").val();
 	console.log(comment)
 	$("#visible-comment").html(comment);
+	// all the rest of our code still goes up here
 	//all your code goes in here
 	return false;
 
 });
+
 });	// here we close the doc ready. no more code below here */
 
