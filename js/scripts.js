a035885313aeb5436b3b771a4ea067519161b13a
Steven Chen <steven20904@yahoo.com>
2015-07-28 05:43:55 -0400

    Remove keyup
    Remove keyup from script.js. Does not work.

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,10 @@
 $(document).ready(function() {
 	alert("Document is ready!");
 	$(".message-box").css("background-color", "yellow");
-});
 	$("#button").on("click", function(){
 	// now we're going to work in here
 	console.log("clicked");
 	return false;
-	});
-	$(".comment-box").on("keyup", function() { ...
-		console.log("keyup happened"); //here we make sure we're catching the keyup
-
-		// in here is where the rest of our code for this lesson will go
 	});
 	var comment = $(".comment-box").val();
 	console.log(comment)
