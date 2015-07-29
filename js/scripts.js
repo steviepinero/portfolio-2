5ed89fde623522de7b94659b66b7b5b7833b417c
Steven Chen <steven20904@yahoo.com>
2015-07-29 05:53:47 -0400

    Remove keyup
    Remove keyup from script.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,10 @@ $(document).ready(function() {
 	// now we're going to work in here
 	console.log("clicked");
 	return false;
+
 	var comment = $(".comment-box").val();
 	console.log(comment)
 	$("#visible-comment").html(comment);
-
 
 
 }); // here we close the doc ready. no more code below here */
