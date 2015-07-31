51704a48cfe0e401ce280812fbd3791edbf50606
Steve Chen <steve@stevechen.me>
2015-07-30 21:24:46 -0400

    Add keyup function

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,20 @@
 
 $(document).ready(function() {
+
 	alert("Document is ready!");
+	$(".comment-box").on("keyup", function() {
+		console.log("keyup happened");
+	});
 	$(".message-box").css("background-color", "yellow");
 	$("#button").on("click", function(){
 	// now we're going to work in here
-	console.log("clicked");
-	return false;
+		console.log("clicked");
 
-	var comment = $(".comment-box").val();
-	console.log(comment)
-	$("#visible-comment").html(comment);
-
-
-}); // here we close the doc ready. no more code below here */
+		var comment = $(".comment-box").val();
+		console.log(comment)
+		$("#visible-comment").html(comment);
+		return false;
+	}); // here we close the doc ready. no more code below here */
 
 });	
 
