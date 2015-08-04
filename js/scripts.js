42fe617d45748b21f1ba518fcf85a9379d2dc732
Steve Chen <steve@stevechen.me>
2015-08-04 00:23:36 -0400

    Add interactive work section

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,23 @@
-$(document).ready(function() {
+var myKitties = [ { title: "First Project", pic: "https://www.petfinder.com/wp-content/uploads/2012/09/Blog-Kitty-Cam-solo.jpg" },
+{ title: "Second Project", pic: "http://www.animal-photography.com/thumbs/red_tabby_long_hair_kitten_~AP-0UJFTC-TH.jpg" }, 
+{ title: "Third Project", pic: "http://www.animal-photography.com/thumbs/silver_tabby_kittens~AP-0JO6Y9-TH.jpg" }, 
+{ title: "Fourth Project", pic: "http://www.animal-photography.com/thumbs/silver_tabby_kitten_looking_up~AP-0DLVMB-TH.jpg" }
+];
 
+$(document).ready(function() {
 	alert("Document is ready!");
+
+	for(var i=0; i<myKitties.length; ++i) {
+		$("#" + i).css("background-image", "url(" + myKitties[i].pic + ")" );
+	};
+
+	$(".image").mouseenter( function() {
+		console.log(this);
+		$(this).html("<p class='info'><span class='proj-title'>Title:</span>" + myKitties[this.id].title + "</p>");
+	}).mouseleave( function() {
+		$("p.info").html("");
+	});
+
 	$(".comment-box").on("keyup", function() {
 		console.log("keyup happened");
 		var charCount = $(".comment-box").val().length;
